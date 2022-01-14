a59a37f5831b1502cd9a9da5a7ca660ae063a7d7
slashrage <tspaeth78@gmail.com>
2022-01-13 18:01:14 -0600

    Adding chair object.

---
+++ 03_10/chair.js
@@ -0,0 +1,11 @@
+class Chair {
+  constructor(height, width, armrests, material, color) {
+    (this.height = height),
+      (this.width = width),
+      (this.armrests = armrests),
+      (this.material = material),
+      (this.color = color);
+  }
+}
+
+export default Chair;
--- 03_10/script.js
+++ 03_10/script.js
@@ -3,6 +3,7 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
 import Backpack from "./Backpack.js";
+import Chair from "./chair.js";
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
@@ -14,5 +15,21 @@ const everydayPack = new Backpack(
   false
 );
 
+const secondaryPack = new Backpack(
+  "Secondary Backpack",
+  20,
+  "purple",
+  3,
+  25,
+  25,
+  true
+);
+
+const chair = new Chair(28, 24, true, "leather", "brown");
+
+const smallChair = new Chair(12, 16, false, "wood", "white stain");
+
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
+console.log("The chair value:", chair);
+console.log("The smaller chair value:", smallChair);
