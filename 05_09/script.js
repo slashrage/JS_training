2bc979f3ce04e0a449edb60ccaeb6240004e9bd2
slashrage <tspaeth78@gmail.com>
2022-01-19 17:13:58 -0600

    lesson 05_09

--- 05_09/script.js
+++ 05_09/script.js
@@ -19,7 +19,6 @@ const everydayPack = new Backpack(
 );
 
 const content = `
-  <article class="backpack" id="everyday">
     <figure class="backpack__image">
       <img src=${everydayPack.image} alt="" />
     </figure>
@@ -50,4 +49,9 @@ const content = `
 
 const main = document.querySelector(".maincontent");
 
-main.innerHTML = content;
+const newArticle = document.createElement("article");
+newArticle.classList.add("backpack");
+newArticle.setAttribute("id", "everyday");
+newArticle.innerHTML = content;
+
+main.append(newArticle);
